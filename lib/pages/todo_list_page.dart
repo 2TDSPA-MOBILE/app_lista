f5abf99300202c5ae83dcd05b6ed691d9788c9fe
Fernando Pinéo <fernandopro1201@gmail.com>
2026-09-15 11:57:23 -0300

    Expanded, ListView, Cores Hexadecimal, Icons

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -15,36 +15,88 @@ class TodoListPage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "E-mail",
-                  hintText: "Digite seu e-mail"
-                ),
-                  //onChanged: onChanged,  
-                  onSubmitted: onSubmitted,             
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Ex: Práticar Flutter"
+                      ),           
+                    ),
+                  ),
+                  SizedBox(width: 8,),
+                  ElevatedButton(
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff7a1111),
+                      padding: EdgeInsets.zero
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                      color:Colors.white
+                    )
+                  )
+                ],
               ),
-              ElevatedButton(
-                onPressed: login, 
-                child: Text("Entrar"))
+              SizedBox(height: 16,),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                    Text("Prof. Fernando"),
+                    ElevatedButton(
+                      onPressed:(){}, child: Text("Pressione")),
+                    Icon(Icons.person,size: 50,),
+                    AppBar(
+                      title: Text("Sou app bar"),
+                      
+                    )
+                  ,
+                  Container(
+                    height: 50,
+                    width: 300,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    color: Colors.yellow,
+                  ),
+                   Container(
+                    height: 50,
+                    width: 300,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    color: Colors.deepOrange,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    color: Colors.purple,
+                  )
+
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(child: Text("Você possui 0 tarefas pendentes")),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff7a1111)),                      
+                    onPressed: (){},
+                    child: Text("Limpar Tudo.",style:TextStyle(color:Colors.white)))
+                ],
+              )
             ],
           ),
         ),
       ),
     );
   }
-  void login(){
-    String text = emailController.text;
-    print("login realizado");
-    print(text);
-    emailController.clear();
-  }
-
-  void onChanged(String text){
-    print(text);
-  }
-
-  void onSubmitted(String text){
-    print(text);
-  }
+  
 }
